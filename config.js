aca6288555261e9766b3c73f672e61d73677b809
chameshLK <panchamd51@gmail.com>
2024-11-28 18:48:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhyWYx5_IFWOx3de2MtnBrwxK_r1gcNhwa6w&s",
+ALIVE_MSG: process.env.ALIVE_MSG || "Hello, I am pancha-md iam alive now!",
 };
